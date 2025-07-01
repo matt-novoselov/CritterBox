b2ec0c49ccd24c6338189529b7630d2330776732
Matvei Novoselov <matveynovoselov@gmail.com>
2025-07-01 21:13:16 +0200

    Init commit

---
+++ PokemonBox/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  PokemonBox
+//
+//  Created by Matt Novoselov on 01/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+
+        let label = UILabel()
+        label.text = "Hello World"
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+
+        view.addSubview(label)
+
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
---
+++ PokemonBoxTests/PokemonBoxTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonBoxTests.swift
+//  PokemonBoxTests
+//
+//  Created by Matt Novoselov on 01/07/25.
+//
+
+import Testing
+@testable import PokemonBox
+
+struct PokemonBoxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
