b5fe0bb78f8bdd9d69d73bb6ff7fe9389633e9e1
Matvei Novoselov <matveynovoselov@gmail.com>
2025-07-02 20:01:04 +0200

    Add image caching

---
+++ PokemonBox/Services/ImageCache.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+/// A simple image cache using NSCache for official artwork images.
+final class ImageCache {
+    static let shared = ImageCache()
+    private let cache = NSCache<NSURL, UIImage>()
+
+    private init() {}
+
+    func image(for url: URL) -> UIImage? {
+        return cache.object(forKey: url as NSURL)
+    }
+
+    func insertImage(_ image: UIImage, for url: URL) {
+        cache.setObject(image, forKey: url as NSURL)
+    }
+}
+
